532425ae4a07834dbca2829b1a53afa33d0a7cc0
unknown <764463703@qq.com>
2019-03-24 23:23:02 +0800

    20190324

---
+++ 3_Decision_tree/trees.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 22:18:17 2019
+
+@author: pingj
+"""
+
+from math import log
+
+def calcShannonEnt(dataSet):
+    #calculate the probability of every label
+    numEntries=len(dataSet)
+    labelCounts={}
+    for featVec in dataSet:
+        currentLabel=featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel]=0
+        labelCounts[currentLabel]+=1
+    #calculate the shannon entropy
+    shannonEnt=0.0
+    for key in labelCounts:
+        prob=float(labelCounts[key])/numEntries
+        shannonEnt-=prob*log(prob,2)
+    return shannonEnt
+
+def createDataSet():
+    dataSet=[[1,1,'yes'],
+             [1,1,'yes'],
+             [1,0,'no'],
+             [0,1,'no'],
+             [0,1,'no']]
+    labels=['no surfacing','flippers']
+    return dataSet,labels
+
