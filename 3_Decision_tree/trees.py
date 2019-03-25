3d464d01f57fe6f99922ecfc8ef0c605de9c507d
unknown <764463703@qq.com>
2019-03-26 01:03:09 +0800

    20190325

--- 3_Decision_tree/trees.py
+++ 3_Decision_tree/trees.py
@@ -6,6 +6,7 @@ Created on Sun Mar 24 22:18:17 2019
 """
 
 from math import log
+import operator
 
 def calcShannonEnt(dataSet):
     #calculate the probability of every label
@@ -32,3 +33,67 @@ def createDataSet():
     labels=['no surfacing','flippers']
     return dataSet,labels
 
+def splitDataSet(dataSet,axis,value):
+    #create a new array to store data after split
+    retDataSet=[]
+    #find suitable data and remove selected axis
+    for featVec in dataSet:
+        if featVec[axis]==value:
+            reducedFeatVec=featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    #get num of features
+    numFeatures=len(dataSet[0])-1
+    #calculate base entropy
+    baseEntropy=calcShannonEnt(dataSet)
+    bestInfoGain=0.0
+    bestFeature=-1
+    #calculate the divided entropy and choose the best entropy increase
+    for i in range(numFeatures):
+        featList=[example[i] for example in dataSet]
+        uniqueVals=set(featList)
+        newEntropy=0.0
+        for value in uniqueVals:
+            subDataSet=splitDataSet(dataSet,i,value)
+            prob=len(subDataSet)/float(len(dataSet))
+            newEntropy+=prob*calcShannonEnt(subDataSet)
+        infoGain=baseEntropy-newEntropy
+        if (infoGain>bestInfoGain):
+            bestInfoGain=infoGain
+            bestFeature=i
+    return bestFeature
+
+def majorityCnt(classList):
+    #get majority label in data set
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote]=0
+        classCount[vote]+=1
+    sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    #get label list
+    classList=[example[-1] for example in dataSet]
+    #if have only one label, return that label
+    if classList.count(classList[0])==len(classList):
+        return classList[0]
+    #if have no feature to split data set, return majority label
+    if len(dataSet[0])==1:
+        return majorityCnt(classList)
+    #find best feature to split data set
+    bestFeat=chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel=labels[bestFeat]
+    myTree={bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues=[example[bestFeat] for example in dataSet]
+    uniqueVals=set(featValues)
+    #divide data set into small data set, and generate tree for leaf node
+    for value in uniqueVals:
+        subLabels=labels[:]
+        myTree[bestFeatLabel][value]=createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
